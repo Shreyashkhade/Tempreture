161f44c8b3bd885f5b2a928c63885fe8ede74560
Shreyashkhade <shreyashkhadepatil@gmail.com>
2023-02-27 13:41:16 +0530

    sk3

---
+++ script.js
@@ -0,0 +1,15 @@
+let celsius = document.getElementById("celsius");
+let fahrenheit = document.getElementById("fahrenheit");
+
+
+function celsiustofahrenheit(){
+    let output = ( parseFloat(celsius.value) * 9/5) + 32;
+    fahrenheit.value = parseFloat( output.toFixed(2));
+    console.log(output);
+}
+
+function fahrenheittocelsius(){
+    let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
+    celsius.value = parseFloat( output.toFixed(2))
+    console.log(output);
+}
